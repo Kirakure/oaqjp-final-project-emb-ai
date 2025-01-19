5b64c3e018c30dac29e508b814de59e74d4a8e97
Kirakure <urbantechtree+github@gmail.com>
2025-01-19 05:03:19 -0500

    submitted

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -7,14 +7,23 @@ def emotion_detector(text_to_analyse):
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     response = requests.post(url, json = myobj, headers=header)
     formatted_response = json.loads(response.text)
-    anger = formatted_response['emotionPredictions'][0]['emotion']['anger']
-    disgust = formatted_response['emotionPredictions'][0]['emotion']['disgust']
-    fear = formatted_response['emotionPredictions'][0]['emotion']['fear']
-    joy = formatted_response['emotionPredictions'][0]['emotion']['joy']
-    sadness = formatted_response['emotionPredictions'][0]['emotion']['sadness']
+    if response.status_code == 200:
+        anger = formatted_response['emotionPredictions'][0]['emotion']['anger']
+        disgust = formatted_response['emotionPredictions'][0]['emotion']['disgust']
+        fear = formatted_response['emotionPredictions'][0]['emotion']['fear']
+        joy = formatted_response['emotionPredictions'][0]['emotion']['joy']
+        sadness = formatted_response['emotionPredictions'][0]['emotion']['sadness']
     
-    emotions = formatted_response['emotionPredictions'][0]['emotion']
-    dominant_emotion = max(emotions, key=emotions.get)
+        emotions = formatted_response['emotionPredictions'][0]['emotion']
+        dominant_emotion = max(emotions, key=emotions.get)
+    elif response.status_code == 400:
+        anger = None
+        disgust = None
+        fear = None
+        joy = None
+        sadness = None
+        dominant_emotion = None
+
     #highest_score = emotions[dominant_emotion]
     return {'anger': anger, 'disgust': disgust, 'fear': fear, 'joy': joy, 
             'sadness': sadness, 'dominant_emotion': dominant_emotion}
--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+""" docstring for server """
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,16 +6,32 @@ app = Flask("Emotion Detector")
 
 @app.route("/")
 def render_index_page():
+    """
+    Render index.html
+    """
     return render_template('index.html')
 
 @app.route("/emotionDetector")
-def sent_emotion():    
+def sent_emotion():
+    """
+    Identify the emotion of a text with Watson AI 
+    """
     text_to_analyze = request.args.get('textToAnalyze')
     response = emotion_detector(text_to_analyze)
     # Extract the label and score from the response
     anger = response['anger']
-    # Return a formatted string with the sentiment label and score
-    return f"For the given statement, the system response is anger: {anger}"
+    disgust = response['disgust']
+    fear = response['fear']
+    joy = response['joy']
+    sadness = response['sadness']
+    dominant_emotion = response['dominant_emotion']
+
+    if dominant_emotion is None:
+        return "Invalid text! Please try again!"
+
+    return f"For the given statement, the system response is 'anger': {anger}, \
+            'disgust': {disgust}, 'fear': {fear}, 'joy': {joy} and \
+            'sadness': {sadness}. The dominant emotion is {dominant_emotion}."
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5050)
